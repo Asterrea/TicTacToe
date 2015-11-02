2c501ce6abf176eee1cd896cae901516817d3455
Asterrea <theresamendoza1115@yahoo.com>
2015-11-02 00:14:57 -0800

    Moving classes

--- src/BoardMove.java
+++ src/BoardMove.java
@@ -13,7 +13,37 @@ public class BoardMove {
 		else if (this.turn == 1) return 0;
 		return turn;
 	}
-
+	
+	 boolean isGameOver(Board b){
+	    	if(!b.label1.isEnabled() && !b.label2.isEnabled() && !b.label3.isEnabled() && 
+	    			(b.label1.getText().equals(b.label2.getText()) && b.label1.getText().equals(b.label3.getText())))
+	    		return true;
+	    	else if(!b.label4.isEnabled() && !b.label5.isEnabled() && !b.label6.isEnabled() &&
+	    			(b.label4.getText().equals(b.label5.getText()) && b.label4.getText().equals(b.label6.getText())))
+	    		return true;
+	    	else if(!b.label7.isEnabled() && !b.label8.isEnabled() && !b.label9.isEnabled() &&
+	    			(b.label7.getText().equals(b.label8.getText()) && b.label7.getText().equals(b.label9.getText())))
+	    		return true;
+	    	else if(!b.label1.isEnabled() && !b.label4.isEnabled() && !b.label7.isEnabled() &&
+	    			(b.label1.getText().equals(b.label4.getText()) && b.label1.getText().equals(b.label7.getText())))
+	    		return true;
+	    	else if(!b.label2.isEnabled() && !b.label5.isEnabled() && !b.label8.isEnabled() &&
+	    			(b.label2.getText().equals(b.label5.getText()) && b.label2.getText().equals(b.label8.getText())))
+	    		return true;
+	    	else if(!b.label3.isEnabled() && !b.label6.isEnabled() && !b.label9.isEnabled() &&
+	    			(b.label3.getText().equals(b.label6.getText()) && b.label3.getText().equals(b.label9.getText())))
+	    		return true;
+	    	
+	    	else if(!b.label1.isEnabled() && !b.label5.isEnabled() && !b.label9.isEnabled() &&
+	    			(b.label1.getText().equals(b.label5.getText()) && b.label1.getText().equals(b.label9.getText())))
+	    		return true;
+	    	else if(!b.label3.isEnabled() && !b.label5.isEnabled() && !b.label7.isEnabled() &&
+	    			(b.label3.getText().equals(b.label5.getText()) && b.label3.getText().equals(b.label7.getText())))
+	    		return true;
+	    	
+			return false;
+	    	
+	    }
 	 
 	public int getTurn() {
 		return turn;
